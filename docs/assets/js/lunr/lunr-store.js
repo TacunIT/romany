2101a8a93e45dac12b1fe214c38acccf37842a97
cs@tacun.it <cs@tacun.it>
2024-05-08 16:57:53 +0200

    Aggiunta la sotto-directory alla URL

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Parallelamente ai social network..",
         "excerpt":"Dobbiamo prevedere una sezione del sito per inserire il diario di bordo, dalla costruzione della barca alla navigazione.  ","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/jekyll/update/post-di-esempio/",
+        "url": "/romany/jekyll/update/post-di-esempio/",
         "teaser": null
       }]
